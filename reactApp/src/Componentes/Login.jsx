ac5a2eac30391a4b2c6abfe1403c999d400d9b13
Mariafrancisca19 <franciscasaborio19@gmail.com>
2024-09-26 16:25:02 -0600

    validacion de los espacios vacios

--- reactApp/src/Componentes/Login.jsx
+++ reactApp/src/Componentes/Login.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { mostrarAlerta } from '../JS/SweetAlert';
 
 const Login = () => {
   const navigate = useNavigate()   
-  
+  const [correo, setCorreo] = useState("")
+  const [password, setPassword] = useState("")
+
+
+  const espaciosVacios=()=>{
+    if (correo.trim() === "" || password.trim() === ""){
+      mostrarAlerta("error","LLENE TODOS LOS CAMPOS")
+    }
+  }
+
+
 
   return (
     <div>
       <h1>BIENVENIDOS</h1>
       <form>
-      <input placeholder="Correo Electrronico" type="email"/> <br/>
-      <input placeholder="Correo Electrronico" type="password"/>
+      <input placeholder="Correo Electrronico" value={correo} type="email" onChange={(e)=>setCorreo(e.target.value)}/> <br/>
+      <input placeholder="Correo Electrronico"  value={password}type="password" onChange={(e)=>setPassword(e.target.value)}/>
       </form>
 
-      <button>REGISTRARSE</button>
+      <button onClick={espaciosVacios}>REGISTRARSE</button>
       <a onClick={()=>{navigate("/Registrarse")}}>NO TENGO CUENTA</a>
     </div>
   )
