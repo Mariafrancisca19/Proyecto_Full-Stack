920ec4d22d898a1a8e7066f4efb6c22fcc2ef9de
Mariafrancisca19 <franciscasaborio19@gmail.com>
2024-09-27 11:44:57 -0600

    creacion del CRUD

---
+++ reactApp/src/JS/Fetch.js
@@ -0,0 +1,97 @@
+const url = "http://localhost:8000/"
+
+// CRUD
+// metodo get extraer los datos
+
+const obtenerDatos = async (enpoint) => {
+    try {
+
+        const peticion = await fetch(`${url}${enpoint}`, {
+            method: "GET",
+            headers: {
+                "Content-type": "application; charset=UTF-8"
+            }
+        });
+
+        const responsa = await responsa.json();
+        return peticion;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export { obtenerDatos }
+
+// metodo post para guardar datos
+
+const guardarDatos = async () => {
+    try {
+
+        const response = await fetch(`${url}${enpoint}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify()
+        });
+
+        return await response.json();
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+export { guardarDatos }
+
+// metodo put para actualizar los datos
+
+const actualizarDatos = async (enpoint, id) => {
+    try {
+
+        const response = await fetch(`${url}${enpoint}/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-type": "application; charset=UTF-8"
+            },
+
+            body: JSON.stringify()
+
+        });
+
+        return await response.json();
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export { actualizarDatos }
+
+// metodo delete para eliminar los datos 
+
+const deleteDatos = async (enpoint, id) => {
+    try {
+        const response = await fetch(`${url}${enpoint}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        return await response.json()
+
+    } catch (error) {
+        console.log(error);
+
+    }
+
+}
+
+
+
+
+
+
+
